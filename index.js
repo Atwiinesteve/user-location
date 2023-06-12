196845810d173d202b928811e40b1fc8c98fde79
KIIZA STEPHEN <53976517+Atwiinesteve@users.noreply.github.com>
2023-06-12 12:23:12 +0000

    added location details with javascript fetching the API

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
+const button = document.querySelector("button");
 
+button.addEventListener("click", () => {
+    navigator.geolocation.getCurrentPosition((position) => {
+        const { longitude, latitude } = position.coords;
+        const url = `https://nominatim.openstreetmap.org/reverse?format=json&lat=${latitude}&lon=${longitude}`;
+        fetch(url)
+            .then((response) => response.json())
+            .then((data) => { console.log(data.address) })
+            .catch((error) => console.log(error))
+    })
+})
