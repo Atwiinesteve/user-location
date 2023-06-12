a3c4c9a49f6490033a68e9047c86eee8a03b8f0e
KIIZA STEPHEN <53976517+Atwiinesteve@users.noreply.github.com>
2023-06-12 12:31:44 +0000

    dynamically added data to client

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const button = document.querySelector("button");
+const area = document.querySelector("#area");
 
 button.addEventListener("click", () => {
     navigator.geolocation.getCurrentPosition((position) => {
@@ -6,7 +7,25 @@ button.addEventListener("click", () => {
         const url = `https://nominatim.openstreetmap.org/reverse?format=json&lat=${latitude}&lon=${longitude}`;
         fetch(url)
             .then((response) => response.json())
-            .then((data) => { console.log(data.address) })
+            .then((data) => area.innerHTML = `
+            
+            <div class="area--details">
+                <div class="area--details--1">
+                    <div class="building">${data.address.building}</div>
+                    <div class="city">${data.address.city}</div>
+                    <div class="country">${data.address.country}</div>
+                    <div class="country__code">${data.address.country_code}</div>
+                    <div class="house__number">${data.address.house_number}</div>
+                </div>
+                <div class="area--details--2">
+                    <div class="neighbourhood">${data.address.neighbourhood}</div>
+                    <div class="road">${data.address.road}</div>
+                    <div class="state">${data.address.state}</div>
+                    <div class="suburb">${data.address.suburb}</div>
+                </div>
+            </div>
+
+            `)
             .catch((error) => console.log(error))
     })
 })
