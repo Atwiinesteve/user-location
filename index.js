b469ae651d93bfb657b2a608f6c525b87c05f929
KIIZA STEPHEN <53976517+Atwiinesteve@users.noreply.github.com>
2023-06-13 08:19:42 +0000

    completed project

--- index.js
+++ index.js
@@ -19,52 +19,55 @@ button.addEventListener("click", () => {
                         </table>
                         <table>
                             <tr>
-                                <th>Building</th>
+                                <th>City</th>
                                 <td>${data.address.city}</td>
                             </tr>
                         </table>
                         <table>
                             <tr>
-                                <th>Building</th>
+                                <th>Country</th>
                                 <td>${data.address.country}</td>
                             </tr>
                         </table>
                         <table>
                             <tr>
-                                <th>Building</th>
-                                <td>${data.address.building}</td>
+                                <th>Country Code</th>
+                                <td>${data.address.country_code}</td>
                             </tr>
                         </table>
                         <table>
                             <tr>
-                                <th>Building</th>
-                                <td>${data.address.building}</td>
+                                <th>House Number</th>
+                                <td>${data.address.house_number}</td>
                             </tr>
-                        </table>
-                            //     <th>City</th>
-                            //     <td>${data.address.building}</td>
-                            //     <th>Country</th>
-                            //     <td>${data.address.building}</td>
-                            //     <th>Country Code</th>
-                            //     <td>${data.address.building}</td>
-                            //     <th>House Number</th>
-                            //     <td>${data.address.building}</td>
-                            // </tr>
                         </table>
                 </div>
                 <div class="area--details--2">
-                    <table>
-                        <tr>
-                            <th>Neighbourhood</th>
-                            <td>${data.address.neighbourhood}</td>
-                            <th>Road</th>
-                            <td>${data.address.road}</td>
-                            <th>State</th>
-                            <td>${data.address.state}</td>
-                            <th>Suburb</th>
-                            <td>${data.address.suburb}</td>
-                        </tr>
-                    </table>
+                        <table>
+                            <tr>
+                                <th>Neighbourhood</th>
+                                <td>${data.address.neighbourhood}</td>
+                            </tr>
+                        </table>
+                        <table>
+                            <tr>
+                                <th>Road</th>
+                                <td>${data.address.road}</td>
+                            </tr>
+                        </table>
+                        <table>
+                            <tr>
+                                <th>State</th>
+                                <td>${data.address.state}</td>
+                            </tr>
+                        </table>
+                        </table>
+                        <table>
+                            <tr>
+                                <th>Suburb</th>
+                                <td>${data.address.suburb}</td>
+                            </tr>
+                        </table>
                 </div>
 
             </div>
